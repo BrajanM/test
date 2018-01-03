0741dfd984a347a38a660eb7b9029a8fe910b345
unknown <student@IBLAB732P-22.ib.polsl.pl>
2018-01-03 15:48:02 +0100

    Initial commit

--- app/src/main/java/com/example/student/kawiarnia/MainActivity.java
+++ app/src/main/java/com/example/student/kawiarnia/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.student.kawiarnia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button wybierz;
+    private EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edit = (EditText)findViewById(R.id.Edit);
+        wybierz = (Button)findViewById(R.id.Pobierz);
+
+        wybierz=(Button)findViewById(R.id.Pobierz);
+
+        wybierz.setOnClickListener(onClick2);
     }
+    private View.OnClickListener onClick2=new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+            MainActivity.this.startActivity(intent);
+            Bundle bundle=new Bundle();
+            intent.putExtras(bundle);
+        }
+    };
 }
